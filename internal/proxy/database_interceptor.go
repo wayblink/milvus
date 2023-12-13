3b40f48b367f060fc39c5ea3a5cfb2e7c065758b
wayblink <anyang.wang@zilliz.com>
2023-12-13 14:54:40 +0800

    27123

--- internal/proxy/database_interceptor.go
+++ internal/proxy/database_interceptor.go
@@ -245,6 +245,9 @@ func fillDatabase(ctx context.Context, req interface{}) (context.Context, interf
 	case *milvuspb.GetCompactionPlansRequest:
 		return ctx, r
 	case *milvuspb.GetFlushStateRequest:
+		if r.DbName == "" {
+			r.DbName = GetCurDBNameFromContextOrDefault(ctx)
+		}
 		return ctx, r
 	case *milvuspb.GetFlushAllStateRequest:
 		return ctx, r
